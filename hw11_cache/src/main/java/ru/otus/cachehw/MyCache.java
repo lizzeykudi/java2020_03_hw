2eb74d525041e75909e5f7b4101cc342b18a7c9e
lizzeykudi <ilovebill>
2020-07-07 19:08:56 +0300

    work on cache

---
+++ hw11_cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -0,0 +1,42 @@
+package ru.otus.cachehw;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.WeakHashMap;
+
+public class MyCache<K, V> implements HwCache<K, V> {
+
+    WeakHashMap<K, V> cache = new WeakHashMap();
+
+    List<SoftReference<HwListener>> listeners = new ArrayList<>();
+
+    @Override
+    public void put(K key, V value) {
+        cache.put(key, value);
+    }
+
+    @Override
+    public void remove(K key) {
+        cache.remove(key);
+    }
+
+    @Override
+    public V get(K key) {
+        return cache.get(key);
+    }
+
+    @Override
+    public void addListener(HwListener<K, V> listener) {
+        listeners.add(new SoftReference<>(listener));
+    }
+
+    @Override
+    public void removeListener(HwListener<K, V> listener) {
+        Optional<SoftReference<HwListener>> softReferenceListener = listeners.stream().filter(hwListenerSoftReference -> hwListenerSoftReference.get().equals(listener)).findAny();
+        if (softReferenceListener.isPresent()) {
+            listeners.remove(softReferenceListener.get());
+        }
+    }
+}
